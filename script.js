7b13e554a4489cf7139ff333383719a416a04dc2
faizan911 <80301546+faizan911@users.noreply.github.com>
2022-12-01 02:08:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-var loaction = document.querySelector(".a");
-var lat = document.querySelector(".b");
-var long = document.querySelector(".c");
-var timezone = document.querySelector(".d");
-var windspeed = document.querySelector(".e");
-var pressure = document.querySelector(".f");
-var humidity = document.querySelector(".g");
-var windDirection = document.querySelector(".h");
-var uv =document.querySelector(".i");
-var feelslike = document.querySelector(".j");
+function getweather() {
+  const location = document.getElementById('city').value;
+  const token = document.getElementById('Token').value;
+  const key = "UVWMTQALQ9BM357GVJ3ZU7846";
 
-function getRequestApi() {
-    // const apidata = "https://api.weatherstack.com/current?access_key=8cbec4b7aed04afe583c09d1d3330bf0&query=India"
-    const apidata = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Texas?unitGroup=metric&key=86TJW5EZKLLZ6RGYVTPV2LUQH&contentType=json"
-    fetch(apidata)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data)=>{
-        console.log(data);
-       
-        var loactionname = data["address"];
-        var Lat = data["latitude"];
-        var time = data["timezone"];
-        var wind = data["currentConditions"]["windspeed"];
-        var pressurevalue = data["currentConditions"]["pressure"];
-        var humidityvalue = data["currentConditions"]["humidity"];
-        var windDirectionvalue = data["currentConditions"]["winddir"]
-        var uvvalue = data["currentConditions"]["uvindex"];
-        var feelslikevalue = data["currentConditions"]["feelslike"];
-        var longvalue = data["longitude"]
-        loaction.innerHTML = `Location: ${loactionname}`
-        lat.innerHTML = `Lat: ${Lat}`
-        timezone.innerHTML = `Time Zone: ${time}`
-        windspeed.innerHTML = `Wind Speed: ${wind}`
-        pressure.innerHTML = `Pressure: ${pressurevalue}`
-        humidity.innerHTML = `Humidity: ${humidityvalue}`
-        windDirection.innerHTML = `Wind Direction: ${windDirectionvalue}`
-        uv.innerHTML = `UV Index: ${uvvalue}`
-        feelslike.innerHTML = `Feels Like: ${feelslikevalue}`
-        long.innerHTML = `Long: ${longvalue}`
+
+  if (location==null || location==''){  
+    alert("Please Enter Location");  
+    return false;  
+  }
+  else if(token==null || token==''){
+    alert("Please Enter Access Token")
+    return false;
+  } 
+  else if(token.length<25){  
+    alert("Access Token must be at least 25 characters long.");  
+    return false;  
+    }  
+
+  console.log(location);
+   var url =`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${token}&contentType=json`;
+ 
+   
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const location = data.address;
+      const lat = data.latitude;
+      const long = data.longitude;
+      const timezone = data.timezone;
+      const windspeed = data.currentConditions.windspeed;
+      const pressure = data.currentConditions.pressure;
+      const humidity = data.currentConditions.humidity;
+      const winddirection = data.currentConditions.winddir;
+      const uv_index = data.currentConditions.uvindex;
+      const feelslike = data.currentConditions.feelslike;
+
+      document.querySelector(".a1").textContent = location;
+      document.querySelector(".a2").textContent = lat;
+      document.querySelector(".a3").textContent = long;
+      document.querySelector(".a4").textContent = timezone;
+      document.querySelector(".a5").textContent = windspeed;
+      document.querySelector(".a6").textContent = pressure;
+      document.querySelector(".a7").textContent = humidity;
+      document.querySelector(".a8").textContent = winddirection;
+      document.querySelector(".a9").textContent = uv_index;
+      document.querySelector(".a10").textContent = feelslike;
     })
 }
